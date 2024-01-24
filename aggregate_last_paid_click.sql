8c4c876f11fde18086bf83535d27fd922e74310a
AlenaVazheina <139154892+AlenaVazheina@users.noreply.github.com>
2024-01-24 22:18:38 +0400

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -65,12 +65,12 @@ left join ads_ya_vk as ads
         and lvl.utm_source = ads.utm_source
         and lvl.utm_medium = ads.utm_medium
         and lvl.utm_campaign = ads.utm_campaign
-    group by
-        lvl.utm_source, lvl.utm_medium, lvl.utm_campaign,
-        ads.total_cost, visit_date
-    order by
-        revenue desc nulls last, visit_date asc,
-        lvl.utm_campaign desc,
-        visitors_count asc, lvl.utm_source asc,
-        lvl.utm_medium asc
+group by
+    lvl.utm_source, lvl.utm_medium, lvl.utm_campaign,
+    ads.total_cost, visit_date
+order by
+    revenue desc nulls last, visit_date asc,
+    lvl.utm_campaign desc,
+    visitors_count asc, lvl.utm_source asc,
+    lvl.utm_medium asc
 limit 15;
